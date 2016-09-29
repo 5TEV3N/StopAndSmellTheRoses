b5d3ee84e377a5787b4dfd92f5b49064cc37e689
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-28 20:27:25 -0400

    Implamented the charecter animation to the game!
    STE-KUN GOO!

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -4,19 +4,40 @@ using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour
 {
-    PlayerController playerController;
-    private float xAxis = 0; // 1 right -1Left
+    PlayerController playerController;          // Refference to playerController
+
+    public SpriteRenderer steKun;               // Refference to SpriteRender
+    public Animator steKunAnimator;             // Animator for Ste-Kun refference
+
+    private float xAxis = 0;                    // 1 = right -1 = Left
 
     void Awake()
     {
         playerController = GameObject.FindGameObjectWithTag("T_Player").GetComponent<PlayerController>();
     }
+
     void FixedUpdate ()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
         if (xAxis != 0)
         {
             playerController.PlayerMove(xAxis);
+            steKunAnimator.SetBool("isRunning", true);
+
+            if (xAxis < 0)
+            {
+                steKun.flipX = true;
+            }
+
+            if (xAxis > 0)
+            {
+                steKun.flipX = false;
+            }
+        }
+
+        if (xAxis == 0)
+        {
+            steKunAnimator.SetBool("isRunning", false);
         }
 
         if (Input.GetKey(KeyCode.Space))
