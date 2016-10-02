4e713209ea7de3b67bd39e5269e65bced72017b9
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-02 00:13:35 -0400

    started working on levelmanager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    //Store Section here. When game is launched, place prefab section into the dessignated section
+    GameObject[] SectionsPrefab;
+    Transform[] SectionsLocation;
+}
