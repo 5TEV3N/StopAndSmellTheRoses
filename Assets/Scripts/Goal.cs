ccc0f4bd531e63dae895384b34ff6454b86187a7
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-24 17:18:33 -0400

    Imported sprite goal, work on it to ping pong and get a better bgm

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,9 +4,13 @@ using System.Collections;
 
 public class Goal : MonoBehaviour
 {
+    void Update()
+    {
+        //lerp here and ping pong
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         SceneManager.LoadScene(2);
     }
-
 }
