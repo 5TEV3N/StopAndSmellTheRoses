e9699caf8a6bcf8db0da39f9dad1ebb938e09119
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-29 20:24:07 -0400

    Working on PingPong

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,9 +4,11 @@ using System.Collections;
 
 public class Goal : MonoBehaviour
 {
+    public Vector3 goalPosition;
     void Update()
     {
-        //lerp here and ping pong
+        Vector3 pingPong = new Vector3 (transform.position.x, Mathf.PingPong(Time.time, 1), transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, pingPong, Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
