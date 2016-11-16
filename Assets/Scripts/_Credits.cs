2e2bb711f34928273935a7bff57d3f93c779dd3b
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-16 03:21:26 -0500

    ??????

---
+++ Assets/Scripts/_Credits.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _Credits : MonoBehaviour
+{
+    /*
+        Camera Follow:
+        https://www.youtube.com/watch?v=5KTemmQQx-E
+
+        https://www.freesound.org/people/klankbeeld/sounds/137070/
+
+        https://www.assetstore.unity3d.com/en/#!/content/33711
+        
+        http://www.looperman.com/loops/detail/90465/guitar-on-a-very-old-record-by-rasputin1963-free-73bpm-chill-out-acoustic-guitar-loop
+
+        http://www.dafont.com/stencil-8bit.font
+    */
+}
